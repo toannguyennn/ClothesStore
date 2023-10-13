71c8f0a609cb4a3c1abb2974c750efa6f1c160fa
NguyenDaiMinhQuan <quandenbp1@gmail.com>
2023-10-13 16:52:52 +0700

    quanquanquan

--- doanflutter/lib/main.dart
+++ doanflutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 //minh quan
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'QuanDemo',
       theme: ThemeData(
         // This is the theme of your application.
         //
