31c9af9a9fec098141a1b97648d5385da8c5ea6c
toannguyennn <toankudo357@gmail.com>
2023-10-19 16:58:00 +0700

    chinh_suaa

--- doanflutter/lib/main.dart
+++ doanflutter/lib/main.dart
@@ -14,7 +14,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       //home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
